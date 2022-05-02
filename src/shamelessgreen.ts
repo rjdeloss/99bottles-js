e8972134e7ce0b872a381dbeba1db1b2692c1487
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-05-02 14:55:37 -0400

    refactoring differentness into sameness

--- src/shamelessgreen.ts
+++ src/shamelessgreen.ts
@@ -32,22 +32,17 @@ class ShamelessGreenBottles {
                     `Take one down and pass it around, ` +
                     `no more bottles of beer on the wall.\n`
                 );
-            case 2:
-                return (
-                    `2 bottles of beer on the wall, ` +
-                    `2 bottles of beer.\n` +
-                    `Take one down and pass it around, ` +
-                    `1 bottle of beer on the wall.\n`
-                );
             default:
                 return (
                     `${number} bottles of beer on the wall, ` +
                     `${number} bottles of beer.\n` +
                     `Take one down and pass it around, ` +
-                    `${number - 1} bottles of beer on the wall.\n`
+                    `${number - 1} ${this.container(number - 1)} of beer on the wall.\n`
                 );
         }
     }
+
+    container = (number: number):string => number === 1 ? "bottle" : "bottles";
 }
 
 export default ShamelessGreenBottles;
